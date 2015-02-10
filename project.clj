dcb82b0b922361cde26d4335afb9dbdf952d2db7
Mike Carter <mike@mikos.co.uk>
2015-02-10 10:55:35 +0000

    Whitespace fixes

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject vijual "0.1.0-SNAPSHOT" 
-  :description "A Graph Layout Library For Clojure" 
+(defproject vijual "0.2.0"
+  :description "A Graph Layout Library For Clojure"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
